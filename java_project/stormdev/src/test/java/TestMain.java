003b46229e90dd92d25267914a920ac6ca88e2f4
余超朱 <rod@yuchaozhudeMacBook-Pro.local>
2019-04-11 15:02:03 +0800

    #test test

--- java_project/stormdev/src/test/java/TestMain.java
+++ java_project/stormdev/src/test/java/TestMain.java
@@ -6,17 +6,5 @@ import java.util.UUID;
 public class TestMain {
     public static void main(String[] args) {
 
-    Integer recordId = 2988; //record.getId().toString();
-    String orgName = "ai-platform"; // record.getOrgName();
-    StringBuilder sb = new StringBuilder(String.format("%s?opsservice=%s#/search?", "https://log-gd9.tools.vipshop.com/", "ai-noah.vip.vip.com"));
-
-    sb.append("_g=(time:(from:now-30m,mode:quick,to:now))&_a=(query:'%28biztype%3A%22flink%22%29+AND+%28namespace%3A%22").
-            append(orgName).append("%22%29+AND+%28bizdomain%3A*.").
-            append("flink-" + recordId).
-            append("." + orgName).
-            append("%29+AND+%28pod%3A%22").
-            append("jm-databoard-flink-2988-k6eqq9-655467f7bf-w8x9s").
-            append("%22%29+AND+%28path%3A*%2Fgc.log.0.current%29',auto:true)");
-        System.out.println(UUID.randomUUID().toString());
     }
 }
